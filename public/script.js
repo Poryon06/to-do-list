33d0f8d28be57896329097525f00c19c9f18de18
Daniel Bretzke <85577050+Poryon06@users.noreply.github.com>
2025-10-16 07:49:22 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,74 @@
+class Task{
+    constructor(description){
+        this.description = description;
+        this.completed = false;
+    }
+
+    toggleComplete(){
+        this.completed = !this.completed;
+    }
+}
+
+class TaskManager{
+    constructor(){
+        this.taskList = document.getElementById('task-list');
+        this.loadTask();
+    }
+
+    async loadTasks(){
+        const response = await fetch('/tasks');
+        const tasks = await response.json();
+        this.render(tasks);
+    }
+
+    async addTask(description){
+        await fetch('/tasks', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json'},
+            body: JSON.stringify({description})
+        });
+        this.loadTasks();
+    }
+
+    async removeTasks(id){
+        await fetch(`/tasks/${id}`, {method: 'DELETE'});
+        this.loadTasks();
+    }
+
+    async toggleTask(id){
+        await fetch (`/tasks/${id}/toggle`, {method:'PATCH'});
+    }
+
+    render(tasks){
+        this.taskList.innerHTML = '';
+
+        tasks.forEach(task => {
+            const li = document.createElement('li');
+            li.className = task.completed ? 'completed' : '';
+
+            const span = document.createElement('button');
+            span.textContent = task.description;
+            span.addEventListener('click', () => this.toggleTask(task.id));
+
+            const removeBtn = document.createElement('button');
+            removeBtn.className = 'remove-btn';
+            removeBtn.textContet = 'Remove';
+            removeBtn.addEventListener('click', () => this.removeTasks(task.id));
+
+            li.appendChild(span);
+            li.appendChild(removeBtn);
+            this.taskList.appendChild(li);
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const TaskManager = new TaskManager();
+    const addTaskBtn = document.getElementById('add-task-btn')
+    const taskInput = document.getElementById('task-input')
+
+    addTaskBtn.addEventListener('click', () => {
+        const taskDescription =  taskInput.value.trim();
+        
+    })
+})
